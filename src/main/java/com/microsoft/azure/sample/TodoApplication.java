8093b00cf835a4501ea35b7515761398f8c2a743
Jason Freeberg <jr.freeberg@comcast.net>
2019-11-19 11:47:28 -0800

    migrated to tomcat for logging

--- src/main/java/com/microsoft/azure/sample/TodoApplication.java
+++ src/main/java/com/microsoft/azure/sample/TodoApplication.java
@@ -6,14 +6,23 @@
 package com.microsoft.azure.sample;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoApplication {
+@EnableAutoConfiguration
+public class TodoApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TodoApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TodoApplication.class);
     }
+
+    // public static void main(String[] args) {
+    //     SpringApplication.run(TodoApplication.class, args);
+    // }
 }
 
 
